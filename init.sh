85909c68330bd0650c44e1aa57ede8b00a91aa51
Connor Knabe <Connor-Knabe@users.noreply.github.com>
2017-10-28 10:24:57 -0500

    Installing node as sudo for pm2

--- init.sh
+++ init.sh
@@ -14,6 +14,17 @@ sleep 1
 echo About to start installing scripts
 sleep 1
 
+echo -n "Have you changed your default password? (y/n)"
+read answer
+if echo "$answer" | grep -iq "^y" ;then
+    echo "It's great to hear that!
+    sleep 2
+else
+    echo "Might be a good idea to do that :)"
+    passwd
+fi
+
+
 
 bashrc="$HOME/.bashrc"
 
@@ -37,9 +48,10 @@ if sudo grep -q "$USER ALL=(ALL) NOPASSWD: ALL" "$sudoers"
       . ~/.bashrc
 fi
 
+#set timezone
+timedatectl set-timezone America/Chicago
 
 mkdir Dev 
-
 mkdir ~/.ssh
 
 authkeys="$HOME/.ssh/authorized_keys"
@@ -49,34 +61,25 @@ then
 else
    touch "$file"
 fi
-
 sudo chmod 400 ~/.ssh/authorized_keys
-
 sudo apt-get update 
-
 sudo apt-get upgrade
-
 sudo apt-get install vim -y
-
 sudo apt-get install zsh -y
+#install node for root user
+curl -sL https://deb.nodesource.com/setup_8.x | sudo -E bash -
+sudo apt-get install nodejs -y
 
 curl -s  -o z.sh https://raw.githubusercontent.com/rupa/z/master/z.sh
-
-timedatectl set-timezone America/Chicago
-
 curl -s  -O https://raw.githubusercontent.com/Connor-Knabe/install-scripts/master/.vimrc
-
 curl -s -o- https://raw.githubusercontent.com/creationix/nvm/v0.33.6/install.sh | bash
 
 . ~/.bashrc
 . ~/.zshrc
 
-nvm install --lts
-
+nvm install 8
 sudo chsh -s $(which zsh)
-
 sh -s -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
-
 curl -s -O https://raw.githubusercontent.com/Connor-Knabe/install-scripts/master/.zshrc
 
 npm install pm2@latest -g
