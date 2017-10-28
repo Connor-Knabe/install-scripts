adf961728650f1c25c81f8dd26033ccb6bdbe557
Connor Knabe <Connor-Knabe@users.noreply.github.com>
2017-10-27 21:08:33 -0500

    Adding directory and naming .z file

--- init.sh
+++ init.sh
@@ -4,6 +4,8 @@ if [[ $EUID -ne 0 ]]; then
    exit 1
 fi
 
+mkdir Dev 
+
 apt-get update 
 
 apt-get upgrade
@@ -12,15 +14,13 @@ apt-get install vim
 
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
 
-curl https://raw.githubusercontent.com/rupa/z/master/z.sh
-
-mv z.sh .z.sh
+curl -o .z.sh https://raw.githubusercontent.com/rupa/z/master/z.sh
 
 timedatectl set-timezone America/Chicago
 
-curl https://raw.githubusercontent.com/Connor-Knabe/install-scripts/master/.zshrc
+curl -O https://raw.githubusercontent.com/Connor-Knabe/install-scripts/master/.zshrc
 
-curl https://raw.githubusercontent.com/Connor-Knabe/install-scripts/master/.vimrc
+curl -O https://raw.githubusercontent.com/Connor-Knabe/install-scripts/master/.vimrc
 
 curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.33.6/install.sh | bash
 
